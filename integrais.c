ee309b2d7cd60c616d086a90ed61596a6a1816be
Manuel Carlos <manuelwn21@gmail.com>
2022-08-05 09:41:42 -0300

    Terceiro termo

--- integrais.c
+++ integrais.c
@@ -13,10 +13,10 @@ int main()
     char escolha[quantidadePolinomios];
 
     printf("Digite a base do primeiro membro do polinomio: ");
-    scanf("%f", &bases[0]);
+    scanf("%f", &bases[1]);
 
-    printf("%.0fx^", bases[0]);          
-    scanf("%f", &expoentes[0]);
+    printf("%.0fx^", bases[1]);          
+    scanf("%f", &expoentes[1]);
 
     if(quantidadePolinomios == 1)
     {
@@ -26,20 +26,66 @@ int main()
 
     if(quantidadePolinomios > 1 && quantidadePolinomios != 1)
     {
-        printf("%.0fx^%.0f ", bases[0], expoentes[0]);
-        scanf("%s", &operacoes[0]);
+        printf("%.0fx^%.0f ", bases[1], expoentes[1]);
+        scanf("%s", &operacoes[1]);
 
-        printf("\nSeu polinomio possui variavel ou constante no segundo termo (V/C)?: ");
-        scanf("%s", &escolha[0]);
-
-        if(escolha[0] == 'C')
+        if(quantidadePolinomios == 2)
         {
-            printf("%.0fx^%.0f %s ", bases[0], expoentes[0], &operacoes[0]);
-            scanf("%f", &bases[1]);
+            printf("\nSeu polinomio possui variavel ou constante no segundo termo (V/C)?: ");
+            scanf("%s", &escolha[1]);
 
-            if(quantidadePolinomios == 2)
+            if(escolha[1] == 'C')
             {
-                printf("\nPolinomio computado:\n%.0fx^%.0f %s %.0f\nEnviando para integracao...\n\n", bases[0], expoentes[0], &operacoes[0], bases[1]);
+                printf("%.0fx^%.0f %s ", bases[1], expoentes[1], &operacoes[1]);
+                scanf("%f", &bases[2]);
+
+                printf("\nPolinomio computado:\n%.0fx^%.0f %s %.0f\nEnviando para integracao...\n\n", bases[1], expoentes[1], &operacoes[1], bases[2]);
+            }
+
+            if(escolha[1] == 'V')
+            {
+                printf("%.0fx^%.0f %s ", bases[1], expoentes[1], &operacoes[1]);
+                scanf("%f", &bases[2]);
+
+                printf("%.0fx^%.0f %s %.0fx^", bases[1], expoentes[1], &operacoes[1], bases[2]);
+                scanf("%f", &expoentes[2]);
+
+                printf("\nPolinomio computado:\n%.0fx^%.0f %s %.0fx^%.0f\nEnviando para integracao...\n\n", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2]);
+            }
+            
+        }
+
+        if(quantidadePolinomios == 3)
+        {
+            printf("%.0fx^%.0f %s ", bases[1], expoentes[1], &operacoes[1]);
+            scanf("%f", &bases[2]);
+
+            printf("%.0fx^%.0f %s %.0fx^", bases[1], expoentes[1], &operacoes[1], bases[2]);
+            scanf("%f", &expoentes[2]);
+
+            printf("%.0fx^%.0f %s %.0fx^%.0f ", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2]);
+            scanf("%s", &operacoes[2]);
+
+            printf("\nSeu polinomio possui variavel ou constante no terceiro termo (V/C)?: ");
+            scanf("%s", &escolha[2]);
+
+            if(escolha[2] == 'C')
+            {
+                printf("%.0fx^%.0f %.1s %.0fx^%.0f %s ", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2], &operacoes[2]);
+                scanf("%f", &bases[3]);
+
+                printf("\nPolinomio computado:\n%.0fx^%.0f %.1s %.0fx^%.0f %s %.0f\nEnviando para integracao...\n\n", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2], &operacoes[2], bases[3]); 
+            }
+
+            if(escolha[2] == 'V')
+            {
+                printf("%.0fx^%.0f %.1s %.0fx^%.0f %s ", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2], &operacoes[2]);
+                scanf("%f", &bases[3]);
+
+                printf("%.0fx^%.0f %.1s %.0fx^%.0f %s %.0fx^", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2], &operacoes[2], bases[3]);
+                scanf("%f", &expoentes[3]);
+
+                printf("\nPolinomio computado:\n%.0fx^%.0f %.1s %.0fx^%.0f %s %.0fx^%.0f\nEnviando para integracao...\n\n", bases[1], expoentes[1], &operacoes[1], bases[2], expoentes[2], &operacoes[2], bases[3], expoentes[3]);
             }
         }
     }
