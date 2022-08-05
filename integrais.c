455572e1754d45064c32d9dc6c58a2c4dcbad51f
Manuel Carlos <manuelwn21@gmail.com>
2022-08-05 01:04:49 -0300

    Em andamento

--- integrais.c
+++ integrais.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    float quantidadeRetangulos, intervalo[1], acumuladorAreaTotal;
+    int quantidadePolinomios;
+    printf("Digite a quantidade de membros do polinomio desejado: ");
+    scanf("%d", &quantidadePolinomios);
 
-    printf("Digite a quantidade de Retangulos: ");
-    scanf("%f", &quantidadeRetangulos);  
+    float bases[quantidadePolinomios], expoentes[quantidadePolinomios];
+    char operacoes[quantidadePolinomios - 1];
+    char escolha[quantidadePolinomios];
 
-    printf("Digite o ponto inicial do intervalo: ");
-    scanf("%f", &intervalo[0]);
+    printf("Digite a base do primeiro membro do polinomio: ");
+    scanf("%f", &bases[0]);
 
-    printf("Digite o ponto final do intervalo: ");
-    scanf("%f", &intervalo[1]);
+    printf("%.0fx^", bases[0]);          
+    scanf("%f", &expoentes[0]);
 
-    for(float i = intervalo[0]; i <= intervalo[1]; )
+    if(quantidadePolinomios == 1)
     {
-        printf("%f", i);
+        printf("\nPolinomio computado:\n%.0fx^%.0f\nEnviando para integracao...\n\n", bases[0], expoentes[0]);
+
     }
 
+    if(quantidadePolinomios > 1 && quantidadePolinomios != 1)
+    {
+        printf("%.0fx^%.0f ", bases[0], expoentes[0]);
+        scanf("%s", &operacoes[0]);
+
+        printf("\nSeu polinomio possui variavel ou constante no segundo termo (V/C)?: ");
+        scanf("%s", &escolha[0]);
+
+        if(escolha[0] == 'C')
+        {
+            printf("%.0fx^%.0f %s ", bases[0], expoentes[0], &operacoes[0]);
+            scanf("%f", &bases[1]);
+
+            if(quantidadePolinomios == 2)
+            {
+                printf("\nPolinomio computado:\n%.0fx^%.0f %s %.0f\nEnviando para integracao...\n\n", bases[0], expoentes[0], &operacoes[0], bases[1]);
+            }
+        }
+    }
 
     system("pause");
     return 0;
